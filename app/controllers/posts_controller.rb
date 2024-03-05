33482dc1f858a6f87736de8f371191c882ed6d3a
Andrew Stupak <andrew.stupak@kellogg.northwestern.edu>
2024-03-05 15:35:51 +0000

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html 
+      format.json { render :json => @post}
+    end
   end
 
   def new
